08d9df4f8ae281611fa8440f7ca08c794d2abcf7
Joshua Sy <z5259375@unsw.edu.au>
2024-05-03 22:08:55 +1000

    added react-bootstrap-icons dep and added profile icon to nav

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from "./styles.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { PersonCircle } from 'react-bootstrap-icons';
+
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.mainNav}>
-      <ul className='main-nav-ul'>
-        <li className='main-nav-li'><a href="/">Home</a></li>
+      <ul>
+        <li ><a href="/">Home</a></li>
         <li><a href="/projects/dashboard">Projects</a></li>
         <li><a href="/events">Events</a></li>
         <li><a href="/people">People</a></li>
-        <li><a href="/profile"><i className="bi bi-person-circle"></i></a></li>
+        <li className="profile-icon"><a href="/profile"><PersonCircle/></a></li>
 
       </ul>
     </nav>
