6533cd1f5ef168387e2b9553de6c262aa1788ffa
Soham Patel <sohampatel271203@gmail.com>
2024-05-26 15:27:41 +1000

    added project description using markdown

--- app/projects/projectpage/page.tsx
+++ app/projects/projectpage/page.tsx
@@ -3,9 +3,27 @@ import Navbar from "@/components/navbar/navbar";
 import Image from "@/components/image/image";
 import TeammateCard from "@/components/teammate/teammate";
 import ProjectName from "@/components/projectname/projectname";
+import Description from "@/components/projectdescription/projectdescription";
 import './page.css';
 
 const App: React.FC = () => {
+  const descriptionContent = `
+    ## About This Project
+
+    This project showcases the top football players around the world, including Neymar, Messi, and Ronaldo.
+
+    ### Features
+
+    - Professional football players
+    - Dribbling, speed, technique
+    - Playmaking, precision
+    - Heading and athleticism
+
+    Enjoy exploring this project!
+
+    **Football Allstars Team**
+  `;
+
   return (
     <div className="preview">
       <Navbar />
@@ -35,6 +53,7 @@ const App: React.FC = () => {
           />
         </div>
       </div>
+      <Description content={descriptionContent} />
     </div>
   );
 }
